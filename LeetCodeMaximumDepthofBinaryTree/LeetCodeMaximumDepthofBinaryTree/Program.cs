37455e94ead684e787d3e81636bc44ca05c233de
Aimal <aimalkhanofficial@gmail.com>
2018-08-19 02:29:12 -0500

    Leet Code

---
+++ LeetCodeMaximumDepthofBinaryTree/LeetCodeMaximumDepthofBinaryTree/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMaximumDepthofBinaryTree
+{
+    class Program
+    {
+        private static int counter = 0;
+        private static List<int> totalValues = new List<int>();
+
+        static void Main(string[] args)
+        {
+             /*
+                Given a binary tree, find its maximum depth.
+                The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+                Note: A leaf is a node with no children.
+             */
+            TreeNode treeNode = new TreeNode(3);
+            treeNode.left = new TreeNode(9);
+            treeNode.right = new TreeNode(20);
+            treeNode.right.right = new TreeNode(7);
+            treeNode.right.left = new TreeNode(15);
+            Console.WriteLine(MaxDepth(treeNode));
+            Console.ReadLine();
+        }
+
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                totalValues.Add(root.val);
+            }
+
+            Console.WriteLine(root.val);
+            MaxDepth(root.left);
+            MaxDepth(root.right);
+
+            if (totalValues.Count <= 2)
+                return (int) totalValues.Count;
+            else
+                return (int) Math.Ceiling((double)totalValues.Count/2);
+        }
+    }
+}
---
+++ LeetCodePathSum/LeetCodePathSum/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePathSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode tree = new TreeNode(1);
+            tree.left = new TreeNode(2);
+            tree.left.left = new TreeNode(3);
+            tree.left.left.left = new TreeNode(4);
+            tree.left.left.left.left = new TreeNode(5);
+            tree.left.left.left.left.left = new TreeNode(6);
+            Console.WriteLine(HasPathSum(tree, 6));
+            Console.ReadLine();
+        }
+
+        public static bool HasPathSum(TreeNode root, int sum)
+        {
+            TreeNode copy = root;
+            int sumFinal = 0;
+            bool isMultipleAdded = false;
+
+            int nodeCounter = 0;
+
+            while (copy != null)
+            {
+                nodeCounter++;
+                sumFinal += copy.val;
+                if (copy.left == null && copy.right == null && sumFinal != sum)
+                {
+                    sumFinal = 0;
+                    copy = DeleteParticularNode(root, copy);
+                    isMultipleAdded = false;
+
+                    //Back to just one node (root)
+                    if (copy == root && (copy.left == null && copy.right== null))
+                    {
+                        break;
+                    }
+                }
+                else if (sumFinal == sum && isMultipleAdded && (copy.left == null && copy.right == null))
+                {
+                    return true;
+                }
+                else
+                {
+                    if (copy.left == null)
+                    {
+                        copy = copy.right;
+                        isMultipleAdded = true;
+                    }
+                    else
+                    {
+                        copy = copy.left;
+                        isMultipleAdded = true;
+                    }
+                }
+            }
+
+            if (nodeCounter == 1 && root.val == sum)
+                return true;
+            return false;
+        }
+
+        public static TreeNode DeleteParticularNode(TreeNode root, TreeNode toDelete)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (root.left == toDelete)
+            {
+                root.left = null;
+            }
+            else if (root.right == toDelete)
+            {
+                root.right = null;
+            }
+
+            DeleteParticularNode(root.left, toDelete);
+            DeleteParticularNode(root.right, toDelete);
+            return root;
+        }
+    }
+}
