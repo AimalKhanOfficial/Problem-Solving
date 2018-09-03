cc00c81fef93b9da20ce109a9a088f55b25d875a
Aimal <aimalkhanofficial@gmail.com>
2018-09-03 12:29:18 -0500

    Stack - Data Structure

---
+++ LeetCodeMinStack/LeetCodeMinStack/MinStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMinStack
+{
+    public class MinStack
+    {
+        private int pointer = -1;
+        private List<int> list;
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+            list = new List<int>();
+        }
+
+        public void Push(int x)
+        {
+            pointer++;
+            list.Add(x);
+        }
+
+        public void Pop()
+        {
+            if (pointer != -1)
+            {
+                list.RemoveAt(pointer);
+                pointer--;
+            }
+        }
+
+        public int Top()
+        {
+            if (pointer != -1)
+            {
+                return list.ElementAt(pointer);
+            }
+            return 0;
+        }
+
+        public int GetMin()
+        {
+            return list.Count > 0 ? list.Min() : 0;
+        }
+    }
+}
---
+++ LeetCodeMinStack/LeetCodeMinStack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMinStack
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            /*
+             *  Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+                push(x) -- Push element x onto stack.
+                pop() -- Removes the element on top of the stack.
+                top() -- Get the top element.
+                getMin() -- Retrieve the minimum element in the stack.
+
+             */
+
+
+            // Your MinStack object will be instantiated and called as such:
+            MinStack obj = new MinStack();
+            obj.Push(1);
+            obj.Push(2);
+            obj.Push(3);
+            obj.Pop();
+            int param_3 = obj.Top();
+            int param_4 = obj.GetMin();
+            Console.WriteLine(param_4);
+            Console.Read();
+        }
+    }
+}
