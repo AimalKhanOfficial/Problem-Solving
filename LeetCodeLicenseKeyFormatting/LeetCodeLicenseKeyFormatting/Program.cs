7471e1e1f02e1171b7ac8e67f30271b00533b57a
Aimal <aimalkhanofficial@gmail.com>
2018-09-03 13:40:16 -0500

    License Number Formatting LeetCode

---
+++ LeetCodeLicenseKeyFormatting/LeetCodeLicenseKeyFormatting/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLicenseKeyFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * You are given a license key represented as a string S which consists only alphanumeric character and dashes. The string is separated into N+1 groups by N dashes.
+
+                Given a number K, we would want to reformat the strings such that each group contains exactly K characters, except for the first group which could be shorter than K, but still must contain at least one character. Furthermore, there must be a dash inserted between two groups and all lowercase letters should be converted to uppercase.
+                
+                Given a non-empty string S and a number K, format the string according to the rules described above.
+                
+                Example 1:
+                
+                Input: S = "5F3Z-2e-9-w", K = 4
+                
+                Output: "5F3Z-2E9W"
+                
+                Explanation: The string S has been split into two parts, each part has 4 characters.
+                Note that the two extra dashes are not needed and can be removed.
+
+             */
+             Console.WriteLine(LicenseKeyFormatting("2", 2));
+            Console.ReadLine();
+        }
+
+        public static string LicenseKeyFormatting(string S, int K)
+        {
+            StringBuilder sb = new StringBuilder();
+            char[] arr = S.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != '-')
+                {
+                    sb.Append(arr[i]);
+                }
+            }
+            string finalString = "";
+            char[] arr2 = sb.ToString().ToUpper().ToCharArray();
+            if (arr2.Length%K == 0)
+            {
+                finalString = GetFinalString(arr2, 0, K);
+            }
+            else
+            {
+                int remainder = arr2.Length%K;
+                for (int i = 0; i < remainder; i++)
+                {
+                    finalString += arr2[i];
+                }
+                if (arr2.Length > 1)
+                {
+                    finalString += "-";
+                    finalString += GetFinalString(arr2, remainder, K);
+                }
+            }
+            return finalString;
+        }
+
+        public static string GetFinalString(char[] arr, int startPoint, int K)
+        {
+            string finalString = "";
+            int div = 0;
+            for (int i = startPoint; i < arr.Length; i++)
+            {
+                if (div > 0 && div % K == 0)
+                {
+                    finalString += "-";
+                    finalString += arr[i];
+                }
+                else
+                {
+                    finalString += arr[i];
+                }
+                div++;
+            }
+            return finalString;
+        }
+    }
+}
---
+++ LeetCodeReverseWordsInAStringIII/LeetCodeReverseWordsInAStringIII/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeReverseWordsInAStringIII
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+             * Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+                Example 1:
+
+                Input: "Let's take LeetCode contest"
+                Output: "s'teL ekat edoCteeL tsetnoc"
+             * 
+             */
+            Console.WriteLine(ReverseWords("Let's take LeetCode contest"));
+            Console.ReadLine();
+        }
+
+        public static string ReverseWords(string s)
+        {
+            string finalString = "";
+            string[] words = s.Split(' ');
+            finalString += ReverseWord(words[0]);
+            for (int i = 1; i < words.Length; i++)
+            {
+                finalString += " " + ReverseWord(words[i]);
+            }
+            return finalString;
+        }
+
+        public static string ReverseWord(string word)
+        {
+            char[] arr = word.ToCharArray();
+            string stringToReturn = "";
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                stringToReturn += arr[i];
+            }
+            return stringToReturn;
+        }
+    }
+}
