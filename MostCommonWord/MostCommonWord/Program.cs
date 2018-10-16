12604d29ea03d293daf3625e02630ecddbbe0c4d
Aimal Khan <Aimal.Khan@us.abb.com>
2018-10-16 15:18:29 -0400

    Some problem solved couple of days ago

---
+++ MostCommonWord/MostCommonWord/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostCommonWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new string[] { "ball" }));
+        }
+
+        public static string MostCommonWord(string paragraph, string[] banned)
+        {
+            
+            return "";
+        }
+    }
+}
