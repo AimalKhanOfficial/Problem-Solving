6c52624fd25ac86225ebecbf40ab6bc3959c5991
Aimal Khan <aimalkhan26@hotmail.com>
2018-11-04 12:13:01 -0800

    LeetCode Problems

---
+++ LeetCodeRotateString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+
+        public bool RotateString(string A, string B) {
+        if (A == B)
+            {
+                return true;
+            }
+
+            for (var i = 0; i < A.Length - 1; i++)
+            {
+                A = A.Substring(1) + A[0].ToString();
+                if (A.Equals(B))
+                {
+                    return true;
+                }
+            }
+            return false;
+    }
+    }
+}
