d16e87b7b24e5c672908da9ec29c859ae02335d8
Aimal Khan <aimalkhan26@hotmail.com>
2018-11-04 12:10:18 -0800

    LeetCode 2 Problems for the day

---
+++ LeetCodeFirstUniqueCharacterInAString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstUniqChar("dddccdbba"));
+            Console.Read();
+        }
+
+        //https://leetcode.com/submissions/detail/187412465/
+        //Run time beats almost 67% of total submissions
+        public static int FirstUniqChar(string s)
+        {
+            if (s.Length == 1) return 0;
+            var isDuplicate = false;
+            for (var i = 0; i < s.Length; i++)
+            {
+                isDuplicate = false;
+                for (var j = 0; j < s.Length; j++)
+                {
+                    if (i == j) continue;
+                    if (s[j] == s[i])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) return i;
+            }
+            return -1;
+        }
+    }
+}
